660802aefee322e2ad427295f51b34ee1e978c23
Geert van Kaathoven <geertvk89@hotmail.com>
2019-06-10 19:37:03 +0000

    Added update_spot @route and edit_spot route

--- app.py
+++ app.py
@@ -42,12 +42,25 @@ def insert_spot():
     
 
 # Edit the spotlist entry into something else #
-@app.route("/edit_spot/<spotlist_id>")
+@app.route('/edit_spot/<spotlist_id>')
 def edit_spot(spotlist_id):
     the_spotlist = mongo.db.spotlist.find_one({"_id": ObjectId(spotlist_id)})
     return render_template('editspot.html', spotlist=the_spotlist)
-                           
-                          
+
+@app.route('/update_spot/<spotlist_id>', methods=["POST"])
+def update_spot(spotlist_id):
+    spotlist = mongo.db.spotlist
+    spotlist.update( {'_id': ObjectId(spotlist_id)},
+    {
+        'animal_name':request.form.get('animal_name'),
+        'animal_description': request.form.get('animal_description'),
+        'date_found': request.form.get('date_found'),
+        'animal_location':request.form.get('animal_location'),
+        'animal_gender':request.form.get('animal_gender'),
+        'animal_image':request.form.get('animal_image')
+    })
+    return redirect(url_for('spotlist_list'))
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
