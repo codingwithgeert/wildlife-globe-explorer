077304ba291037d29af259cc8da4b7138e7fe953
Errepulify <kahalzer.kok3@gmail.com>
2019-07-28 15:40:07 +0000

    Added test_get_animals to see if the function GET works

--- test_app.py
+++ test_app.py
@@ -9,10 +9,12 @@ class FlaskClientTestCase(unittest.TestCase):
     def setUp(self):
         self.app = app.app
         self.client = self.app.test_client()
+        
     def test_get_animals(self):
         result = self.client.get('/spotlist_list')
         self.assertTrue(result.status_code == 200)
         self.assertIn(b'Boar', result.data)
+        self.assertIn(b'Roodborstje', result.data)
      
 
         
